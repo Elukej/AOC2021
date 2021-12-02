19590c2c55ef925767fb8209f6f3ed859af8d4b2
Bludni <elukej@gmail.com>
2021-12-02 10:02:55 +0100

    small improvement to day1

--- day1/second.cpp
+++ day1/second.cpp
@@ -12,20 +12,15 @@ int main()
     input.open("input.txt");
     std::string line1;
 
-    for(int i = 0; i<4; i++) {
+    for(int i = 0; i<3; i++) {
         std::getline(input, line1);
         t[i] = std::stoi(line1);
     }
-    sum1 = t[0] + t[1] + t[2];
-    sum2 = t[1] + t[2] + t[3];
-    if (sum2 > sum1) count_inc++;
 
     while (std::getline(input, line1)) {
-        sum1 = sum2;
-        for (int i = 0; i < 3; i++) t[i] = t[i+1];
         t[3] = std::stoi(line1);
-        sum2 += (t[3] - t[0]);
-        if (sum2 > sum1) count_inc++;
+        if (t[3] > t[0]) count_inc++;
+        for (int i = 0; i < 3; i++) t[i] = t[i+1];
     };
     std::cout << count_inc << std::endl;
 }
